46a8b657732fd918d6175ed433b2bfad81433afd
Tony Deng <wolf.deng@gmail.com>
2018-09-29 16:09:25 +0800

    :sparkles: feat(1.0): 添加healthcheck-sofa-boot依赖和实现
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ src/main/java/com/github/tonydeng/demo/sofaboot/samples/SofaBootSamplesApplication.java
@@ -0,0 +1,11 @@
+package com.github.tonydeng.demo.sofaboot.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBootSamplesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SofaBootSamplesApplication.class, args);
+	}
+}
